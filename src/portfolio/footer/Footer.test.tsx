fa2c07625e1718ae5c6cd51b4c4a1ad79c6fd613
r41ph <dev@rgalan.es>
2023-09-01 18:52:16 +0100

    test links open in a new tab

--- src/portfolio/footer/Footer.test.tsx
+++ src/portfolio/footer/Footer.test.tsx
@@ -21,4 +21,22 @@ describe("Footer", () => {
     expect(codepenLink.getAttribute("href")).toBe("https://codepen.io/r41ph");
     expect(contactLink.getAttribute("href")).toContain("mailto:jobs@rgalan.es");
   });
+
+  test("should open the links in a new tab", () => {
+    render(<Footer />);
+    const githubLink = screen.getByRole("link", {
+      name: "Link to r41ph github account",
+    });
+    const codepenLink = screen.getByRole("link", {
+      name: "Link to r41ph codepen account",
+    });
+    const contactLink = screen.getByRole("link", {
+      name: "Send email to r41ph",
+    });
+    expect(githubLink).toHaveAttribute("target", "_blank");
+    expect(codepenLink).toHaveAttribute("target", "_blank");
+    expect(contactLink).toHaveAttribute("target", "_blank");
+  });
+
+  // test
 });
