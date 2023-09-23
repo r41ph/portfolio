ff6a91c380bd9e05346ff0f0f1ecc8e637e271cc
r41ph <dev@rgalan.es>
2023-09-23 18:49:24 +0100

    undo vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,23 @@ export default defineConfig({
     react({ plugins: [["@swc/plugin-styled-components", {}]] }),
     macrosPlugin(),
     svgr(),
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    eslint(),
+    {
+      // default settings on build (i.e. fail on error)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      ...eslint(),
+      apply: "build",
+    },
+    {
+      // do not fail on serve (i.e. local development)
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false,
+        exclude: [/virtual:/, /node_modules/],
+      }),
+      apply: "serve",
+      enforce: "post",
+    },
   ],
   css: {
     postcss: {
