cfdccf654057ba834dfb484373707659409fb1de
r41ph <dev@rgalan.es>
2023-10-10 12:27:50 +0100

    move frontend to /client

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node scripts/serve.js
--- scripts/serve.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(express.static("dist"));
-app.listen(port, () => console.log(`Listening on port ${port}!`));
