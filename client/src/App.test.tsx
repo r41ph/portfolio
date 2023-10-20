104e7d8c98e8617bd21d698c39f54ab60043cd76
r41ph <dev@rgalan.es>
2023-10-20 21:09:11 +0100

    update App test to use msw

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,9 +3,11 @@ import App from "./App";
 import { renderWithRouter } from "./test-utils/utils";
 
 describe("App", () => {
-  test("App rendering", () => {
+  test("App rendering", async () => {
     renderWithRouter(<App />);
-    expect(screen.getByText(/mock project/i)).toBeInTheDocument();
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    const mockProject = await screen.findByText(/mock project/i);
+    expect(mockProject).toBeInTheDocument();
   });
 
   test("App navigating", async () => {
