2de97ceb06603560fb1e68b6b2d0b5337622ef92
r41ph <dev@rgalan.es>
2023-10-20 21:08:34 +0100

    setup msw and handlers

---
+++ client/src/test-utils/msw.handlers.ts
@@ -0,0 +1,50 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("https://api.unsplash.com/photos/random", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          urls: { small: "testSrc" },
+          user: {
+            name: "testAuthorName",
+            links: { self: "testAuthorUrl" },
+          },
+        },
+      ]),
+    );
+  }),
+  rest.get("api/projects", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        projects: [
+          {
+            name: "Mock project",
+            stack: ["HTML", "CSS", "JAVASCRIPT"],
+            company: "Syrox",
+            image: "/images/projects/green-sheep-thumbnail.jpg",
+          },
+        ],
+      }),
+    );
+  }),
+  rest.get("*", (req, res, ctx) => {
+    console.error(`Please add request handler for ${req.url.toString()}`);
+    return res(
+      ctx.status(500),
+      ctx.json({ error: "You must add request handler." }),
+    );
+  }),
+];
+// {
+//     projects: [
+//       {
+//         name: "Mock project",
+//         stack: [Icons.HTML, Icons.CSS, Icons.JAVASCRIPT],
+//         company: "Syrox",
+//         image: "/images/projects/green-sheep-thumbnail.jpg",
+//       },
+//     ],
+//   }
--- client/src/test-utils/setupTest.ts
+++ client/src/test-utils/setupTest.ts
@@ -1,10 +1,20 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
+import { server } from "./node.js";
 import "@testing-library/jest-dom";
 import "./mocks";
 import "./utils";
 
-// runs a cleanup after each test case (e.g. clearing jsdom)
+beforeAll(() => {
+  server.listen();
+});
+
 afterEach(() => {
+  server.resetHandlers();
+  // runs a cleanup after each test case (e.g. clearing jsdom)
   cleanup();
+});
+
+afterAll(() => {
+  server.close();
 });
