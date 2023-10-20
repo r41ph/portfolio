cfe0bafcc6344ef011c8e1aad105aa8af5daa43b
r41ph <dev@rgalan.es>
2023-10-20 21:09:39 +0100

    update test callback mock to use vi.fn()

--- client/src/components/Label/Label.test.tsx
+++ client/src/components/Label/Label.test.tsx
@@ -7,10 +7,10 @@ describe("Label", () => {
   test("displays the text and icon passed to it", () => {
     render(<Label text="test" icon={IconType.GITHUB} />);
     expect(screen.getByText("test")).toBeInTheDocument();
-    expect(screen.getByTestId(IconType.GITHUB)).toBeInTheDocument();
+    expect(screen.getByTestId(/github/i)).toBeInTheDocument();
   });
   test("calls the callback function when clicked", async () => {
-    const mockCallback = jest.fn();
+    const mockCallback = vi.fn();
     render(<Label text="test" callback={mockCallback} />);
     await userEvent.click(screen.getByText("test"));
     expect(mockCallback).toHaveBeenCalled();
