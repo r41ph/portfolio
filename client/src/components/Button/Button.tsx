9fffdc290b521c619365ba6f8f1ac8f618022cae
r41ph <dev@rgalan.es>
2023-10-31 12:34:46 +0000

    implement React aria useHover

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { AriaButtonProps, useButton } from "@react-aria/button";
 import { useFocusRing } from "@react-aria/focus";
-// import { useHover } from "@react-aria/interactions";
+import { useHover } from "@react-aria/interactions";
 import { mergeProps } from "@react-aria/utils";
 import { HTMLAttributes, useRef } from "react";
 
@@ -14,14 +14,15 @@ export function Button(props: ButtonProps) {
   const { children, className, tabIndex } = props;
   const { buttonProps } = useButton(props, ref);
   const { isFocusVisible, focusProps } = useFocusRing();
-  const mergedAriaProps = mergeProps(buttonProps, focusProps);
-
+  const { hoverProps, isHovered } = useHover(props);
+  const mergedAriaProps = mergeProps(buttonProps, focusProps, hoverProps);
   return (
     <button
       {...mergedAriaProps}
       className={`${className ?? ""} ${isFocusVisible ? "focus-ring" : ""}`}
       ref={ref}
       tabIndex={tabIndex ?? 0}
+      style={{ opacity: isHovered ? 0.6 : 1 }}
     >
       {children}
     </button>
