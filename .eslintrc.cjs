fc280f9dcfc373868fda5f7071c992c094f93acd
r41ph <dev@rgalan.es>
2023-11-15 22:35:22 +0000

    Fix formatting issues in .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,18 +2,19 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2020: true,
+    es2020: true
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module",
+    sourceType: "module"
     // project: true,
   },
   plugins: ["prettier"],
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
-    "prettier/prettier": "error",
-  },
+    "prettier/prettier": ["error", { trailingComma: "none" }],
+    "comma-dangle": "off"
+  }
 };
