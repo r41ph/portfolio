1064993b7c183e40f44acf3d558d9394c77c0fbf
r41ph <dev@rgalan.es>
2023-11-15 22:36:01 +0000

    Add VITE_JWT_TOKEN to ImportMetaEnv

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,4 +4,9 @@ declare module "vite-plugin-eslint";
 
 interface ImportMetaEnv {
   readonly VITE_UNSPLASH_ACCESS_KEY: string;
+  readonly VITE_JWT_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
