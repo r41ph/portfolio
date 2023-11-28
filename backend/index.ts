94575dfd87aeb47893724cbafaf4057cc728434e
r41ph <dev@rgalan.es>
2023-11-28 11:54:13 +0000

    Update static file paths in backend/index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -53,15 +53,15 @@ app.use("/data", cors());
 app.use("/auth", authRoutes);
 app.use("/data", apiRoutes);
 
-app.use(express.static(__dirname + "/dist"));
-app.use(express.static(__dirname + "/dist/storybook"));
+app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "/storybook"));
 
 app.use("/storybook", (_req, res) => {
-  res.sendFile(path.join(__dirname, "dist/storybook/index.html"));
+  res.sendFile(path.join(__dirname, "/storybook/index.html"));
 });
 
 app.use("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 mongoConnect(() => {
