e426c8c7a0059c6dd69864c6497081e0632f0991
r41ph <dev@rgalan.es>
2023-11-29 19:11:06 +0000

    Update static file serving configuration

--- backend/index.ts
+++ backend/index.ts
@@ -3,15 +3,15 @@ import compression from "compression";
 import { mongoConnect } from "./utils/database";
 import cors from "cors";
 import bodyParser from "body-parser";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+// import path, { dirname } from "path";
+// import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 
 import { router as apiRoutes } from "./routes/dataRoutes";
 import { router as authRoutes } from "./routes/authRoutes";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 app.use(compression());
@@ -76,16 +76,16 @@ app.use("/data", cors());
 app.use("/auth", authRoutes);
 app.use("/data", apiRoutes);
 
-app.use(express.static(__dirname + "/"));
-app.use(express.static(__dirname + "/storybook"));
+// app.use(express.static(__dirname + "/"));
+// app.use(express.static(__dirname + "/storybook"));
 
-app.use("/storybook", (_req, res) => {
-  res.sendFile(path.join(__dirname, "/storybook/index.html"));
-});
+// app.use("/storybook", (_req, res) => {
+//   res.sendFile(path.join(__dirname, "/storybook/index.html"));
+// });
 
-app.use("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
+// app.use("*", (_req, res) => {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
 
 mongoConnect(() => {
   app.listen(process.env.PORT || 3000);
