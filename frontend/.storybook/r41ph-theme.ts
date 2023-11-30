addee071de96c116376d35270841f89802dbfc3b
r41ph <dev@rgalan.es>
2023-11-30 18:10:25 +0000

    Update Storybook theme

---
+++ frontend/.storybook/r41ph-theme.ts
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "r41ph's Storybook",
+  brandUrl: "https://ralph.es",
+  brandImage: "https://ralph.es/images/logo.png",
+});
