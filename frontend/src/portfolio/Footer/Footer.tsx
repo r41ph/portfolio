3f1ef1b08307efcaceac3b161575502bee24dac5
r41ph <dev@rgalan.es>
2023-12-04 12:56:53 +0000

    Update footer margin for small screens

--- frontend/src/portfolio/Footer/Footer.tsx
+++ frontend/src/portfolio/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { getYear } from "../../utils/date";
 
 export const Footer = () => {
   return (
-    <footer className="mt-40 h-9 ">
+    <footer className="mt-12 sm:mt-40 h-9 ">
       <div className="flex justify-center">
         <p className="text-sm dark:animate-dark-fade-in">
           © {getYear()} r41ph. All rights reserved.
