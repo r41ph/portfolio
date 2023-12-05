42173556536f8d17c29b0ce3431b41321ed1567a
r41ph <dev@rgalan.es>
2023-12-05 10:33:22 +0000

    Add SelectOptions component with react-select

---
+++ frontend/src/components/Form/SelectOptions/SelectOptions.tsx
@@ -0,0 +1,24 @@
+import Select, { GroupBase, Props } from "react-select";
+
+export function SelectOptions<
+  Option,
+  IsMulti extends boolean = false,
+  Group extends GroupBase<Option> = GroupBase<Option>,
+>(props: Props<Option, IsMulti, Group>) {
+  return (
+    <Select
+      name={props.name}
+      className="project-type-select"
+      options={props.options}
+      defaultValue={props.defaultValue}
+      isMulti={props.isMulti}
+      styles={{
+        control: (baseStyles) => ({
+          ...baseStyles,
+          padding: "6px",
+          border: "1px solid black",
+        }),
+      }}
+    />
+  );
+}
