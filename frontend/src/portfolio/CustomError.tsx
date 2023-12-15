4f0abdd334704e48962cbe6b4d7f01111f9c5241
r41ph <dev@rgalan.es>
2023-12-15 12:44:51 +0000

    Refactor CustomError component to handle different types of errors

--- frontend/src/portfolio/CustomError.tsx
+++ frontend/src/portfolio/CustomError.tsx
@@ -1,11 +1,31 @@
-import { useRouteError } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from "react-router-dom";
 
-export function CustomError({ message }: { message: string }) {
+export function CustomError() {
   const error = useRouteError();
-  console.log("🚀 ~ file: CustomError.tsx:5 ~ CustomError ~ error:", error);
+  const navigate = useNavigate();
+  let errorMessage: string | number;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.status || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold text-gray-900">{message}</h1>
+      <h1 className="text-2xl font-bold text-gray-900">Oops!</h1>
+      <h2 className="text-lg font-bold text-gray-900 my-4">
+        {errorMessage ? errorMessage : "An error occurred. Please try again."}
+      </h2>
+      <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
   );
 }
