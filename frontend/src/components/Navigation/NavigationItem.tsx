2120d91151d30477c020b43464778bdf75fcbb39
r41ph <dev@rgalan.es>
2024-01-04 13:15:12 +0000

    Refactor NavigationItem component to accept `end` prop

--- frontend/src/components/Navigation/NavigationItem.tsx
+++ frontend/src/components/Navigation/NavigationItem.tsx
@@ -1,9 +1,22 @@
 import { NavLink } from "react-router-dom";
 
-export function NavigationItem({ to, name }: { to: string; name: string }) {
+export function NavigationItem({
+  to,
+  name,
+  classNames,
+  end = false,
+}: {
+  to: string;
+  name: string;
+  classNames?: string;
+  end?: boolean;
+}) {
+  const isEnd = end ? { end: true } : {};
   return (
-    <li className="m-w-10 font-semibold">
-      <NavLink to={to}>{name}</NavLink>
+    <li className={`${classNames ? classNames : ""} m-w-10 font-semibold`}>
+      <NavLink to={to} {...isEnd}>
+        {name}
+      </NavLink>
     </li>
   );
 }
