1cfd0bf16deef735102d0b4c2aa66a7d03fde8d2
r41ph <dev@rgalan.es>
2024-01-04 13:34:45 +0000

    Add styled component for SuccessMessage

---
+++ frontend/src/dashboard/SuccessMessage.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import tw, { css } from "twin.macro";
+
+export const SuccessMessage = styled.p(() => [
+  tw`
+    flex 
+    flex-col 
+    items-center
+  `,
+  css`
+    .icon {
+      margin-top: 20px;
+      path {
+        stroke: green;
+      }
+    }
+  `,
+]);
